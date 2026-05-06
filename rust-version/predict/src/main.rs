911edbe7a2b14dc0d592d89d84930316c8a06ebe
5a-lil <khalilhadjmes1@gmail.com>
2026-05-06 12:40:11 +0200

    Finished predict program for rust-version, need to do the bonuses and its finished

---
+++ rust-version/predict/src/main.rs
@@ -0,0 +1,34 @@
+use std::io::{BufRead, stdin};
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufReader};
+
+fn read_thetas(file_path: &str) -> Result<(f64, f64), Box<dyn Error>> {
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+    let (mut theta0, mut theta1) = (0.0, 0.0);
+
+    for line in reader.lines() {
+        let line = line?;
+        if line.starts_with("theta0:") {
+            theta0 = line.split(':').nth(1).unwrap().trim().parse()?;
+        } else if line.starts_with("theta1:") {
+            theta1 = line.split(':').nth(1).unwrap().trim().parse()?;
+        }
+    }
+
+    Ok((theta0, theta1))
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let (theta0, theta1) = read_thetas(".thetas")?;
+
+    let mut input = String::new();
+    stdin().read_line(&mut input)?;
+    let mileage: f64 = input.trim().parse()?;
+
+    let price = theta0 + theta1 * mileage;
+    println!("km: {mileage} -> price: {price}");
+
+    Ok(())
+}
