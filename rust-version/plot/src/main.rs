afebe0ae689f411d238165f093526b2ff6687137
5a-lil <khalilhadjmes1@gmail.com>
2026-05-08 00:24:31 +0200

    Finished plot, now need of calculation of precision of the regression

--- rust-version/plot/src/main.rs
+++ rust-version/plot/src/main.rs
@@ -1,6 +1,43 @@
+use csv::Reader;
 use plotters::prelude::*;
+use std::io::BufRead;
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufReader};
+
+fn read_thetas(file_path: &str) -> Result<(f64, f64), Box<dyn Error>> {
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+    let (mut theta0, mut theta1) = (0.0, 0.0);
+
+    for line in reader.lines() {
+        let line = line?;
+        if line.starts_with("theta0:") {
+            theta0 = line.split(':').nth(1).unwrap().trim().parse()?;
+        } else if line.starts_with("theta1:") {
+            theta1 = line.split(':').nth(1).unwrap().trim().parse()?;
+        }
+    }
+
+    Ok((theta0, theta1))
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file_path = "../data.csv";
+    
+    let mut rdr = Reader::from_path(file_path)?;
+    let mut value_i: Vec<f64> = vec![];
+    let mut value_j: Vec<f64> = vec![];
+    
+    for result in rdr.records() {
+        let record = result?;
+        value_i.push(record[0].trim().parse::<f64>()?);
+        value_j.push(record[1].trim().parse::<f64>()?);
+    }
+
+    let i_max = value_i.iter().fold(f64::NEG_INFINITY, |a, &b| a.max(b));
+    let (theta0, theta1) = read_thetas(".thetas").unwrap();
+
     let root = BitMapBackend::new("line.png", (800, 600)).into_drawing_area();
     root.fill(&WHITE)?;
     
@@ -9,11 +46,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .margin(10)
         .x_label_area_size(40)
         .y_label_area_size(40)
-        .build_cartesian_2d(0.0..3.0, 0.0..10.0)?;
+        .build_cartesian_2d(0.0..240000.0, 0.0..9000.0)?;
     
     chart.configure_mesh().draw()?;
     
-    let points = vec![(0.0, 0.0), (1.0, 1.0), (2.0, 4.0), (3.0, 9.0)];
+    chart.draw_series(
+        value_i.iter().zip(value_j).map(|(x, y)| Cross::new((*x, y), 4, &BLUE))
+    )?;
+
+    let points = vec![(0.0, theta0), (i_max, theta1 * i_max + theta0)];
     chart.draw_series(LineSeries::new(points, &RED))?;
 
     Ok(())
